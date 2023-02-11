b32676876a230eb3c3aaf99e6938b6f300f7376b
radnom <maxtam642@gmail.com>
2023-02-11 21:04:28 +0800

    general

---
+++ Leetcode/LC2/solution.py
@@ -0,0 +1,74 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = 0
+        num2 = 0
+        i = 0
+        while l1 != None or l2 != None:
+            if l1 != None:
+                num1 += l1.val*(10**i)
+                l1 = l1.next
+            if l2 != None:
+                num2 += l2.val*(10**i)
+                l2 = l2.next
+            i += 1
+        num3 = num1+num2
+        l3 = ListNode(num3 % 10)
+        tmp = l3
+        num3 //= 10
+        while num3 != 0:
+            tmp.next = ListNode(num3 % 10)
+            num3 = num3//10
+            tmp = tmp.next
+        return l3
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l3 = ListNode(0)
+        tmp = l3
+        carry = 0
+        while l1 != None or l2 != None:
+            total = carry
+            if l1 != None:
+                total += l1.val
+                l1 = l1.next
+            if l2 != None:
+                total += l2.val
+                l2 = l2.next
+            carry = total//10
+            tmp.next = ListNode(total % 10)
+            tmp = tmp.next
+        if carry != 0:
+            tmp.next = ListNode(carry)
+        return l3.next
+
+
+class Solution:
+    l3 = ListNode(0)
+    tmp = l3
+    carry = 0
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        total = self.carry
+        if l1 != None:
+            total += l1.val
+        if l2 != None:
+            total += l2.val
+        if total == 0 and l1 == None and l2 == None:
+            return self.l3.next
+
+        self.carry = total//10
+        self.tmp.next = ListNode(total % 10)
+        self.tmp = self.tmp.next
+
+        if l1 != None and l2 != None:
+            self.addTwoNumbers(l1.next, l2.next)
+        elif l1 != None:
+            self.addTwoNumbers(l1.next, None)
+        elif l2 != None:
+            self.addTwoNumbers(None, l2.next)
+        elif self.carry > 0:
+            self.tmp.next = ListNode(self.carry)
+            self.tmp = self.tmp.next
+
+        return self.l3.next
---
+++ Leetcode/LC69/solution.py
@@ -0,0 +1,85 @@
+import math
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(x**0.5)
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(math.sqrt(x))
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.pow(x, 0.5)
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        guess=1
+        while guess*guess < x: # O(logn)
+            guess += 1
+        return guess-1 if guess*guess != x else guess
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 0:
+            return 0
+        left=1
+        right=x
+        while True:
+            mid=left+(right-left)//2 # O(logn)
+            if mid*mid == x:
+                return mid
+            elif mid*mid > x:
+                right=mid
+            else:
+                if (mid+1)*(mid+1) > x:
+                    return mid
+                left=mid
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 0:
+            return 0
+        left=1
+        right=x
+        while True:
+            mid=left+(right-left)//2 # O(logn)
+            if mid > x/mid:
+                right=mid
+            elif mid < x/mid:
+                if (mid+1) > x/(mid+1):
+                    return mid
+                left=mid
+            else:
+                return mid
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int: # O(logn)
+        root=x
+        while root*root > x:
+            root=int((root+x/root)/2)
+        return root
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ans=0
+        mask=1 << 16
+        while mask > 0: # O(1)
+            ans |= mask
+            if ans*ans > x:
+                ans ^= mask
+            mask >>= 1
+        return ans
+
+
+if __name__ == "__main__":
+    x=81
+    print(Solution().mySqrt(x))
