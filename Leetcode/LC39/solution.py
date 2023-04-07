6ea4b6b14e9c817bd2707c005e301f36791ca06f
radnom <maxtam642@gmail.com>
2023-04-07 10:13:32 +0800

    more

---
+++ Leetcode/LC39/solution.py
@@ -0,0 +1,50 @@
+class Solution:
+    def helper(self, arr, candidates, target):
+        for i in range(len(candidates)):
+            candidate = candidates[i]
+            if candidate == target:
+                self.final.append(arr+[candidate])
+            if target-candidate > 0:
+                self.helper(arr+[candidate], candidates[i:], target-candidate)
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.final = []
+        self.helper([], candidates, target)
+        return self.final
+
+
+class Solution:
+    def helper(self, arr, candidates, target):
+        for i in range(len(candidates)):
+            candidate = candidates[i]
+            if candidate == target:
+                self.final.append(arr+[candidate])
+            if candidate > target:
+                return
+            if target-candidate > 0:
+                self.helper(arr+[candidate], candidates[i:], target-candidate)
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.final = []
+        candidates.sort()
+        self.helper([], candidates, target)
+        return self.final
+
+# any other method to solve this problem?
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        final = []
+        def helper(arr, candidates, target):
+            for i in range(len(candidates)):
+                candidate = candidates[i]
+                if candidate == target:
+                    final.append(arr+[candidate])
+                if candidate > target:
+                    return
+                if target-candidate > 0:
+                    helper(arr+[candidate], candidates[i:], target-candidate)
+        helper([], candidates, target)
+        return final
