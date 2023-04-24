f341c7eed22f29fdd2a3908066565dc10d243f77
radnom <maxtam642@gmail.com>
2023-04-24 23:12:04 +0800

    some sql

--- Leetcode/LC176/solution.sql
+++ Leetcode/LC176/solution.sql
@@ -1,1 +1,4 @@
 SELECT Max(Salary) as SecondHighestSalary FROM Employee WHERE salary < (SELECT MAX(Salary) FROM Employee)
+SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary
+SELECT Salary FROM (SELECT Salary, dense_rank() OVER (ORDER BY Salary DESC) AS rank FROM Employee) AS t WHERE rank = 2
--- Leetcode/LC180/solution.sql
+++ Leetcode/LC180/solution.sql
@@ -1,7 +1,3 @@
-# Write your MySQL query statement below
-SELECT distinct l1.num as ConsecutiveNums
-from 
-logs l1, logs l2, logs l3
-where
-l1.id=l2.id-1 and l2.id=l3.id-1
-and l1.num=l2.num and l2.num=l3.num
+SELECT distinct l1.num as ConsecutiveNums from logs l1, logs l2, logs l3 where l1.id=l2.id-1 and l2.id=l3.id-1 and l1.num=l2.num and l2.num=l3.num
+
+SELECT DISTINCT num AS ConsecutiveNums FROM Logs WHERE (Id+1,num) in (SELECT * FROM Logs) AND (Id+2,num) in (SELECT * FROM Logs)
