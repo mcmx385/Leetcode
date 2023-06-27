4e62cf25ba69cbb7b6b3e87ddfd62b617e9f89a8
maxtam642 <maxtam642@gmail.com>
2023-06-27 08:44:02 +0800

    Update solution.py

--- Leetcode/LC62/solution.py
+++ Leetcode/LC62/solution.py
@@ -34,3 +34,30 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         self.matrix = [[0 for _ in range(n)] for _ in range(m)]
         return self.helper(m, n)
+
+
+class Solution:
+    def factorial(self, n):
+        if n == 0:
+            return 1
+        return n * self.factorial(n - 1)
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        return int(
+            self.factorial(m + n - 2) / self.factorial(m - 1) / self.factorial(n - 1)
+        )
+
+
+class Solution:
+    def factorial(self, n):
+        if n == 0:
+            return 1
+        if self.saved[n - 1] != 0:
+            return n * self.saved[n - 1]
+        return n * self.factorial(n - 1)
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        self.saved = [0 for _ in range(m + n - 2)]
+        return int(
+            self.factorial(m + n - 2) / self.factorial(m - 1) / self.factorial(n - 1)
+        )
