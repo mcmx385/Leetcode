3b561eac9c0a945ef958eb3d7c657c6a9d8454e6
maxtam642 <maxtam642@gmail.com>
2023-07-04 13:00:50 +0800

    Create solution.py

---
+++ Leetcode/LC137/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mapp = {}
+        for num in nums:
+            if num not in mapp:
+                mapp[num] = 1
+            else:
+                mapp[num] += 1
+        for num in mapp:
+            if mapp[num] == 1:
+                return num
+        return -1
+    
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mapp = {}
+        for num in nums:
+            if num not in mapp:
+                mapp[num] = False
+            else:
+                mapp[num] = True
+        return [num for num in mapp if not mapp[num]][0]
