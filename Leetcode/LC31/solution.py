fca554ced391c29b8ba9ede4ebc1383c7574307c
maxtam642 <maxtam642@gmail.com>
2023-07-04 13:54:36 +0800

    Update solution.py

--- Leetcode/LC31/solution.py
+++ Leetcode/LC31/solution.py
@@ -38,3 +38,22 @@ class Solution:
             print(nums)
             return
         return self.helper(nums, 0)
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = len(nums) - 2
+        while left >= 0 and nums[left] >= nums[left + 1]:
+            left -= 1
+        if left == -1:
+            nums.sort()
+            return
+        right = len(nums) - 1
+        while right >= 0 and nums[right] <= nums[left]:
+            right -= 1
+        nums[left], nums[right] = nums[right], nums[left]
+        nums[left + 1 :] = nums[left + 1 :][::-1]
+        return
